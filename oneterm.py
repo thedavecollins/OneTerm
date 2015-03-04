1b4c8220fd338aa9129cec37ce22afa9806ef492
Dave Collins <dave@hopest.net>
2015-03-04 14:56:27 +1100

    Inital config

--- oneterm.py
+++ oneterm.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import sys
+import os
+from ConfigParser import SafeConfigParser
 import pango
 import gtk
 import vte
@@ -9,13 +11,25 @@ class OneTerm(gtk.Window):
     def __init__(self):
         super(OneTerm, self).__init__()
 
-        self.set_title("OneTerm")
+        config = SafeConfigParser()
+
+
+        configFileName = os.path.join(os.path.expanduser('~/.onetermrc'))
+        config.add_section('style')
+        config.set('style', 'font', 'Monospace' )
+        config.set('style', 'fontsize', '12' )
+        config.set('style', 'backgroundcolor', '#000' )
+
+        if os.path.exists(configFileName):
+            config.read(configFileName)
+
+        
+        self.set_title("OneTerm " )
         self.connect("destroy", self.on_destroy)
 
         font = pango.FontDescription()
-        font.set_family("Monospace")
-        # font size
-        font.set_size(14 * pango.SCALE)
+        font.set_family(config.get('style','font',False))
+        font.set_size(config.getint('style', 'fontsize') * pango.SCALE)
         font.set_weight(pango.WEIGHT_NORMAL)
         font.set_stretch(pango.STRETCH_NORMAL)
 
@@ -25,10 +39,10 @@ class OneTerm(gtk.Window):
         terminal.set_encoding("UTF-8")
         terminal.set_font_full(font, True)
 
-        terminal.fork_command("bash")
+        terminal.fork_command(os.environ["SHELL"])
 
         self.fullscreen()                                                                                                                               
-        color = gtk.gdk.Color(0,0,0)
+        color = gtk.gdk.color_parse(config.get("style", "backgroundcolor"))
         self.modify_bg(gtk.STATE_NORMAL, color)
         self.f  = gtk.Fixed()
         self.add(self.f)
